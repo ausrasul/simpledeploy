b0763390bc7e0628acce334fd0f2dd98d4cc9a66
Aus Abdalrasul <ausrasul@gmail.com>
2024-01-17 14:13:37 +0100

    Added support for: - Json configs - multiple ports - volumes - servcies - custome path and filename for repository config file.

--- simpledeploy.py
+++ simpledeploy.py
@@ -1,11 +1,128 @@
 import os
 import subprocess
 import configparser
+import json
 
-def read_config(filename):
+def this_script_dir():
+    return os.path.dirname(os.path.realpath(__file__))
+
+def read_ini_config(filename):
     config = configparser.ConfigParser()
     config.read(filename)
     return {k: v for k, v in config.items()}
+
+def read_config(filename):
+    # reads json config file
+    with open(filename, 'r') as f:
+        config = json.load(f)
+    return config
+
+class Repo:
+    def __init__(self, config_file):
+        script_dir = this_script_dir()
+        config_file = os.path.join(script_dir, config_file)
+        config = read_config(config_file)
+        
+        self.dir = os.path.join(script_dir, config['repo']['dir'])
+        self.url = config['repo']['url']
+        self.mount_dir = config['repo']['mount_dir']
+        self.cfg_file = config['repo']['cfg_file']
+
+class Container:
+    def __init__(self, cfg):
+        self.name = cfg['name']
+        self.image = cfg.get('image', 'ubuntu:latest')
+        self.command = cfg.get('command')
+        self.ports = cfg.get('ports', [])
+        self.volumes = cfg.get('volumes', [])
+        self.work_dir = cfg.get('work_dir')
+
+    def _remove(self):
+        try:
+            subprocess.run(['podman', 'rm', '-f', self.name], check=True)
+            print(f"Container {self.name} removed.")
+        except subprocess.CalledProcessError as e:
+            print(f"An error occurred: {e.stderr}")
+    def stop(self):
+        try:
+            subprocess.run(['podman', 'stop', self.name], check=True)
+            print(f"Container {self.name} stopped.")
+        except subprocess.CalledProcessError as e:
+            print(f"An error occurred: {e.stderr}")
+        finally:
+            self._remove()
+    
+    def start(self, podname):
+        command = [
+            'podman', 'run', '-d',
+            '--name', self.name,
+            '--pod', podname,
+        ]
+        for volume in self.volumes:
+            command.extend(['-v', volume])
+        if self.work_dir:
+            command.extend(['-w', self.work_dir])
+        command.append(self.image)
+        if self.command:
+            command.extend(['bash', '-c', self.command])
+        print(command)
+        subprocess.run(command, check=True)
+
+
+class App:
+    def __init__(self, repo):
+        script_dir = this_script_dir()
+        abs_file_name = os.path.join(script_dir, repo.dir, repo.cfg_file)
+        config = read_config(abs_file_name)
+
+        self.name = config['name']
+
+        app = config.get('app', {})
+        volumes = app.get('volumes',[])
+        volumes.extend([
+            f"{repo.dir}:{repo.mount_dir}"
+        ])
+        app['volumes'] = volumes
+        app['work_dir'] = repo.mount_dir
+        self.container = Container(app)
+
+        services = config.get('services', [])
+        self.services = []
+        for service in services:
+            self.services.append(Container(service))
+    
+    def _create_pod(self):
+        command = ['podman', 'pod', 'create', '--name', self.name]
+        for port in self.container.ports:
+            command.extend(['-p', port])
+        for service in self.services:
+            for port in service.ports:
+                command.extend(['-p', port])
+        subprocess.run(command, check=True)
+    def _remove_pod(self):
+        command = ['podman', 'pod', 'rm', '-f', self.name]
+        for _ in range(3):  # Retry the command 3 times
+            try:
+                subprocess.run(command, check=True, timeout=5)
+                break  # Break the loop if the command succeeds
+            except:
+                continue  # Retry the command if it fails
+
+    def start(self):
+        self._create_pod()
+        self._services_start()
+        self.container.start(podname=self.name)
+    def stop(self):
+        self._remove_pod()
+        self.container.stop()
+        self._services_stop()
+    def _services_start(self):
+        for service in self.services:
+            print(service)
+            service.start(podname=self.name)
+    def _services_stop(self):
+        for service in self.services:
+            service.stop()
 
 def clone_repo_if_not_exist(repo_name, repo_dir):
     if not os.path.exists(repo_dir):
@@ -27,53 +144,19 @@ def git_clone_or_pull(repo_name, repo_dir):
         this_hash = subprocess.run(['git', 'rev-parse', 'HEAD'], stdout=subprocess.PIPE, text=True).stdout.strip()
         return prev_hash,this_hash
 
-def remove_container(container_name):
-    try:
-        subprocess.run(['podman', 'rm', '-f', container_name], check=True)
-        print(f"Container {container_name} stopped successfully.")
-    except subprocess.CalledProcessError as e:
-        print(f"An error occurred: {e.stderr}")
-
-def stop_and_remove_container(container_name):
-    try:
-        subprocess.run(['podman', 'stop', container_name], check=True)
-        print(f"Container {container_name} stopped successfully.")
-    except subprocess.CalledProcessError as e:
-        print(f"An error occurred: {e.stderr}")
-    finally:
-        remove_container(container_name)
 
 def main():
-    # Get the directory of the current script
-    script_dir = os.path.dirname(os.path.realpath(__file__))
-    config_file = os.path.join(script_dir, 'config.ini')
-    script_reader = os.path.join(script_dir, 'read_config.py')
+    repo = Repo('config.json')
+    prev_hash, this_hash = git_clone_or_pull(repo.url, repo.dir)
 
-    # Source the configuration file
-    repo_config = read_config(config_file)['REPO']
-    # Check if the repo directory exists
-    repo_dir = os.path.join(script_dir, repo_config['REPO_DIR'])
-    repo_mount_dir = repo_config['REPO_MOUNT_DIR']
-
-    prev_hash, this_hash = git_clone_or_pull(repo_config['REPO_NAME'], repo_dir)
-
-    os.chdir(repo_dir)
+    os.chdir(repo.dir)
     print(prev_hash, this_hash)
     # Check if there are any new commits since the last run
     if prev_hash != this_hash:
-        ci_config = read_config('./ci.ini')['CI']
-        print(ci_config['CI_COMMAND'])
-        # check if podman container sexists
-        stop_and_remove_container(ci_config['CI_NAME'])
-        subprocess.run(['podman', 'rm', '-f', ci_config['CI_NAME']])
-        subprocess.run(['podman', 'run',
-                        '-d',
-                        '-p', ci_config['CI_PORT'],
-                        '-v', f"{repo_dir}:{repo_mount_dir}",
-                        '-w', repo_mount_dir,
-                        '--name', ci_config['CI_NAME'],
-                        ci_config['CI_IMAGE'],
-                        'bash', '-c', ci_config['CI_COMMAND']], check=True)
+        app = App(repo)
+        app.stop()
+        app.start()
+
 
 if __name__ == '__main__':
     main()
